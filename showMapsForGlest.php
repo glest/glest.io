a1e6836436ff6ab72c9acb1e79283fac2cb61d60
mathusummut <mathusum.mut@gmail.com>
2019-03-03 14:04:34 +0100

    Added test mod

--- showMapsForGlest.php
+++ showMapsForGlest.php
@@ -1,1 +1,2 @@
 cliff_way.mgm|8|117865711||http://mods.megaglest.org/maps/cliff_way.mgm|http://mods.megaglest.org/maps/cliff_way.mgm.jpg|
+lasagna.mgm|8|117865711||http://mods.megaglest.org/maps/cliff_way.mgm|http://mods.megaglest.org/maps/cliff_way.mgm.jpg|
