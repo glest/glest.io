de59929d5b1c7551d1fae4f6434e311dbba4e19f
mathusummut <mathusum.mut@gmail.com>
2019-03-03 14:05:59 +0100

    Removed test mod

--- showMapsForGlest.php
+++ showMapsForGlest.php
@@ -1,2 +1,1 @@
 cliff_way.mgm|8|117865711||http://mods.megaglest.org/maps/cliff_way.mgm|http://mods.megaglest.org/maps/cliff_way.mgm.jpg|
-lasagna.mgm|8|117865711||http://mods.megaglest.org/maps/cliff_way.mgm|http://mods.megaglest.org/maps/cliff_way.mgm.jpg|
