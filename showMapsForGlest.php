eb922cb33dbe443015da821e5fdf3ed03d2a0302
mathusummut <mathusum.mut@gmail.com>
2019-03-05 23:04:07 +0100

    Added map mods

--- showMapsForGlest.php
+++ showMapsForGlest.php
@@ -1,3 +1,8 @@
-stranded.mgm|4|0|Stranded Map from VBros Pack 1|https://glest.io/mods/maps/stranded.mgm|https://glest.io/mods/maps/stranded.jpg|
-the_blocked_path.mgm|4|0|The Blocked Path Map from VBros Pack 1|https://glest.io/mods/maps/the_blocked_path.mgm|https://glest.io/mods/maps/the_blocked_path.jpg|
-the_island.mgm|4|0|The Island Map from VBros Pack 1|https://glest.io/mods/maps/the_island.mgm|https://glest.io/mods/maps/the_island.jpg|
+2_cities|8|0|2 Cities Map by JammyJamJamMan|https://glest.io/mods/maps/2_cities.mgm|https://glest.io/mods/maps/2_cities.jpg|
+lost_river|5|0|Lost River Map by JammyJamJamMan|https://glest.io/mods/maps/lost_river.mgm|https://glest.io/mods/maps/lost_river.jpg|
+mountain_pass|6|0|Mountain Pass Map by JammyJamJamMan|https://glest.io/mods/maps/mountain_pass.mgm|https://glest.io/mods/maps/mountain_pass.jpg|
+paths_to_war|4|0|Paths to War Map by JammyJamJamMan|https://glest.io/mods/maps/paths_to_war.mgm|https://glest.io/mods/maps/paths_to_war.jpg|
+ravine_crossing|4|0|Ravine Crossing Map by JammyJamJamMan|https://glest.io/mods/maps/ravine_crossing.mgm|https://glest.io/mods/maps/ravine_crossing.jpg|
+stranded|4|0|Stranded Map from VBros Pack 1|https://glest.io/mods/maps/stranded.mgm|https://glest.io/mods/maps/stranded.jpg|
+the_blocked_path|4|0|The Blocked Path Map from VBros Pack 1|https://glest.io/mods/maps/the_blocked_path.mgm|https://glest.io/mods/maps/the_blocked_path.jpg|
+the_island|4|0|The Island Map from VBros Pack 1|https://glest.io/mods/maps/the_island.mgm|https://glest.io/mods/maps/the_island.jpg|
