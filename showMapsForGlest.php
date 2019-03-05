47dfc9015bca66b66337de85ea104e08444037f5
mathusummut <mathusum.mut@gmail.com>
2019-03-06 00:35:46 +0100

    Fixed map mods

--- showMapsForGlest.php
+++ showMapsForGlest.php
@@ -1,8 +1,8 @@
-2_cities|8|3081592918|2 Cities Map by JammyJamJamMan|https://glest.io/mods/maps/2_cities.mgm|https://glest.io/mods/maps/2_cities.jpg|
-lost_river|5|354542947|Lost River Map by JammyJamJamMan|https://glest.io/mods/maps/lost_river.mgm|https://glest.io/mods/maps/lost_river.jpg|
-mountain_pass|6|2290923836|Mountain Pass Map by JammyJamJamMan|https://glest.io/mods/maps/mountain_pass.mgm|https://glest.io/mods/maps/mountain_pass.jpg|
-paths_to_war|4|1595034854|Paths to War Map by JammyJamJamMan|https://glest.io/mods/maps/paths_to_war.mgm|https://glest.io/mods/maps/paths_to_war.jpg|
-ravine_crossing|4|932833782|Ravine Crossing Map by JammyJamJamMan|https://glest.io/mods/maps/ravine_crossing.mgm|https://glest.io/mods/maps/ravine_crossing.jpg|
-stranded|4|2266272119|Stranded Map from VBros Pack 1|https://glest.io/mods/maps/stranded.mgm|https://glest.io/mods/maps/stranded.jpg|
-the_blocked_path|4|480174676|The Blocked Path Map from VBros Pack 1|https://glest.io/mods/maps/the_blocked_path.mgm|https://glest.io/mods/maps/the_blocked_path.jpg|
-the_island|4|178958721|The Island Map from VBros Pack 1|https://glest.io/mods/maps/the_island.mgm|https://glest.io/mods/maps/the_island.jpg|
+2_cities.mgm|8|3081592918|2 Cities Map by JammyJamJamMan|https://glest.io/mods/maps/2_cities.mgm|https://glest.io/mods/maps/2_cities.jpg|
+lost_river.mgm|5|354542947|Lost River Map by JammyJamJamMan|https://glest.io/mods/maps/lost_river.mgm|https://glest.io/mods/maps/lost_river.jpg|
+mountain_pass.mgm|6|2290923836|Mountain Pass Map by JammyJamJamMan|https://glest.io/mods/maps/mountain_pass.mgm|https://glest.io/mods/maps/mountain_pass.jpg|
+paths_to_war.mgm|4|1595034854|Paths to War Map by JammyJamJamMan|https://glest.io/mods/maps/paths_to_war.mgm|https://glest.io/mods/maps/paths_to_war.jpg|
+ravine_crossing.mgm|4|932833782|Ravine Crossing Map by JammyJamJamMan|https://glest.io/mods/maps/ravine_crossing.mgm|https://glest.io/mods/maps/ravine_crossing.jpg|
+stranded.mgm|4|2266272119|Stranded Map from VBros Pack 1|https://glest.io/mods/maps/stranded.mgm|https://glest.io/mods/maps/stranded.jpg|
+the_blocked_path.mgm|4|480174676|The Blocked Path Map from VBros Pack 1|https://glest.io/mods/maps/the_blocked_path.mgm|https://glest.io/mods/maps/the_blocked_path.jpg|
+the_island.mgm|4|178958721|The Island Map from VBros Pack 1|https://glest.io/mods/maps/the_island.mgm|https://glest.io/mods/maps/the_island.jpg|
