11dc774e0a1970a051c4b4c727233d31f4e673dc
mathusummut <mathusum.mut@gmail.com>
2019-03-05 23:51:27 +0100

    Updated CRCs

--- showTechsForGlest.php
+++ showTechsForGlest.php
@@ -1,14 +1,14 @@
-enemies_and_allies|1|0|Enemies and Allies beta7\n\nby Eliminator|https://glest.io/mods/techs/enemies_and_allies.7z|https://glest.io/mods/techs/enemies_and_allies.png|
-imperial_m_g|3|0|Imperial Faction is a medieval based faction with steampunk influences.\nby Mr War\n\n (tech and magic included)|https://glest.io/mods/techs/imperial_m_g.7z|https://glest.io/mods/techs/imperial_m_g.jpg|
-assault_command|2|0|An Epic Modern War Tech tree,\nA very epic tree to play with other people|https://glest.io/mods/techs/assault_command.7z|https://glest.io/mods/techs/assault_command.jpg|
-munchy|2|0|Here is a techtree of two factions:\n* Monsters and\n* Aliens\n\n made by Noah Vejvoda ( tiger )|https://glest.io/mods/techs/munchy.7z|https://glest.io/mods/techs/munchy.jpg|
-nihilirian|2|0|A story of two distinctively different races with one origin - Arcturus & Sirius.\nhttp://home.arcor.de/nihilirian\n\n   Made by Hristo Ilchev |https://glest.io/mods/techs/nihilirian.7z|https://glest.io/mods/techs/nihilirian.jpg|
-elves_tech|3|0|An Elves Faction\n\nCredits: Gameboy, Archmage, MirceaKitsune, Battle For Wesnoth [Music]|https://glest.io/mods/techs/elves_tech.7z|https://glest.io/mods/techs/elves_tech.jpg|
-sci_fi_pack|2|0|Martians - Industarons\n>> the scici pack <<  \n\nby mr WAR|https://glest.io/mods/techs/sci_fi_pack.7z|https://glest.io/mods/techs/sci_fi_pack.jpg|
-vbros_pack_1|4|0|The VBros pack 1 includes four factions:\n* Pirates\n* Western\n* British\n* Canada|https://glest.io/mods/techs/vbros_pack_1.7z|https://glest.io/mods/techs/vbros_pack_1.jpg|
-vbros_pack_2|4|0|The VBros pack 2 includes four factions:\n* Dark-Knights\n* Gauls\n* Crusaders\n* Greek|https://glest.io/mods/techs/vbros_pack_2.7z|https://glest.io/mods/techs/vbros_pack_2.jpg|
-vbros_pack_3|4|0|The VBros pack 3 includes four factions:\n* Chess\n* Circus\n* Mario\n* Penguin|https://glest.io/mods/techs/vbros_pack_3.7z|https://glest.io/mods/techs/vbros_pack_3.jpg|
-vbros_pack_4|4|0|The VBros pack 4 includes four factions:\n* USA (United States of America)\n* Moon\n* Martians\n* PC|https://glest.io/mods/techs/vbros_pack_4.7z|https://glest.io/mods/techs/vbros_pack_4.jpg|
-vbros_pack_5|4|0|The VBros pack 5 includes four factions:\n* Aztec\n* Dinosaur\n* Africa\n* Barbarians|https://glest.io/mods/techs/vbros_pack_5.7z|https://glest.io/mods/techs/vbros_pack_5.jpg|
-darkpack|8|0|A Dark Magic Faction, By John DH|https://glest.io/mods/techs/darkpack.7z|https://glest.io/mods/techs/darkpack.jpg|
-zombie_invasion|3|0|A Minecraft Mod by LinuxPlayer|https://glest.io/mods/techs/zombie_invasion.7z|https://glest.io/mods/techs/zombie_invasion.jpg|
+enemies_and_allies|1|12567543|Enemies and Allies beta7\n\nby Eliminator|https://glest.io/mods/techs/enemies_and_allies.7z|https://glest.io/mods/techs/enemies_and_allies.png|
+imperial_m_g|3|2601389364|Imperial Faction is a medieval based faction with steampunk influences.\nby Mr War\n\n (tech and magic included)|https://glest.io/mods/techs/imperial_m_g.7z|https://glest.io/mods/techs/imperial_m_g.jpg|
+assault_command|2|1731496093|An Epic Modern War Tech tree,\nA very epic tree to play with other people|https://glest.io/mods/techs/assault_command.7z|https://glest.io/mods/techs/assault_command.jpg|
+munchy|2|1830121919|Here is a techtree of two factions:\n* Monsters and\n* Aliens\n\n made by Noah Vejvoda ( tiger )|https://glest.io/mods/techs/munchy.7z|https://glest.io/mods/techs/munchy.jpg|
+nihilirian|2|3329203541|A story of two distinctively different races with one origin - Arcturus & Sirius.\nhttp://home.arcor.de/nihilirian\n\n   Made by Hristo Ilchev |https://glest.io/mods/techs/nihilirian.7z|https://glest.io/mods/techs/nihilirian.jpg|
+elves_tech|3|877878866|An Elves Faction\n\nCredits: Gameboy, Archmage, MirceaKitsune, Battle For Wesnoth [Music]|https://glest.io/mods/techs/elves_tech.7z|https://glest.io/mods/techs/elves_tech.jpg|
+sci_fi_pack|2|1710562622|Martians - Industarons\nthe scifi\n\nby Mr War|https://glest.io/mods/techs/sci_fi_pack.7z|https://glest.io/mods/techs/sci_fi_pack.jpg|
+vbros_pack_1|4|3538698396|The VBros pack 1 includes four factions:\n* Pirates\n* Western\n* British\n* Canada|https://glest.io/mods/techs/vbros_pack_1.7z|https://glest.io/mods/techs/vbros_pack_1.jpg|
+vbros_pack_2|4|3803925785|The VBros pack 2 includes four factions:\n* Dark-Knights\n* Gauls\n* Crusaders\n* Greek|https://glest.io/mods/techs/vbros_pack_2.7z|https://glest.io/mods/techs/vbros_pack_2.jpg|
+vbros_pack_3|4|31488519|The VBros pack 3 includes four factions:\n* Chess\n* Circus\n* Mario\n* Penguin|https://glest.io/mods/techs/vbros_pack_3.7z|https://glest.io/mods/techs/vbros_pack_3.jpg|
+vbros_pack_4|4|1375373135|The VBros pack 4 includes four factions:\n* USA (United States of America)\n* Moon\n* Martians\n* PC|https://glest.io/mods/techs/vbros_pack_4.7z|https://glest.io/mods/techs/vbros_pack_4.jpg|
+vbros_pack_5|4|3642513775|The VBros pack 5 includes four factions:\n* Aztec\n* Dinosaur\n* Africa\n* Barbarians|https://glest.io/mods/techs/vbros_pack_5.7z|https://glest.io/mods/techs/vbros_pack_5.jpg|
+darkpack|8|4211530611|A Dark Magic Faction, By John DH|https://glest.io/mods/techs/darkpack.7z|https://glest.io/mods/techs/darkpack.jpg|
+zombie_invasion|3|2963311747|A Minecraft Mod by LinuxPlayer|https://glest.io/mods/techs/zombie_invasion.7z|https://glest.io/mods/techs/zombie_invasion.jpg|
--- showTilesetsForGlest.php
+++ showTilesetsForGlest.php
@@ -1,2 +1,2 @@
-lego_land|0|A Lego theme tileset|https://glest.io/mods/tilesets/lego_land.7z|https://glest.io/mods/tilesets/lego_land.jpg|
-mother_board|0|Play on a Motherboard\nby Eliminator|https://glest.io/mods/tilesets/mother_board.7z|https://glest.io/mods/tilesets/mother_board.jpg|
+lego_land|535582801|A Lego theme tileset|https://glest.io/mods/tilesets/lego_land.7z|https://glest.io/mods/tilesets/lego_land.jpg|
+mother_board|877404888|Play on a Motherboard\nby Eliminator|https://glest.io/mods/tilesets/mother_board.7z|https://glest.io/mods/tilesets/mother_board.jpg|
